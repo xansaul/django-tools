36d33cab27c704ff2d4aebd9d89ee64b5b6b4624
Huerta Murillo Alejandro Saul <saul901_@hotmail.com>
2024-04-29 00:54:56 -0600

    rest_framework added in settings.py

--- resources_generator/src/project/create_project.rs
+++ resources_generator/src/project/create_project.rs
@@ -1,4 +1,4 @@
-use std::{path::PathBuf, process::Command};
+use std::{fs, path::PathBuf, process::Command};
 use cli_logic::{cli::ProjectAction, utils::create_folder};
 use super::utils::{exect_commands, get_python_command};
 
@@ -24,15 +24,11 @@ pub fn create_project(args: &ProjectAction)-> Result<&'static str, Box<dyn std::
 
 
     if args.rest_framework {
-        println!("Installing REST framework...");
-        
-        if let Err(error) = exect_commands(
-            build_command_install_rest_framework(&complete_path_project)
-        ) {
+
+        if let Err(error) = add_rest_framework(&complete_path_project, &args.name) {
             return Err(error);
         }
-    
-
+        
     }
 
     Ok("Project created.")
@@ -95,15 +91,12 @@ fn build_command_install_rest_framework(complete_path_project: &PathBuf)->String
 
 fn build_python_command_for_create_project(complete_path_project: &PathBuf, project_name: &String)->String{
 
-
-
     let command = format!(
         r#"pip install django && django-admin startproject {} {}"#,
         project_name,
         complete_path_project.to_str().unwrap()
     );
 
-    
     build_command(complete_path_project, command)
 }
 
@@ -120,4 +113,50 @@ fn get_command_to_activate_venv(complete_path_project: &PathBuf)-> String{
     command
 }
 
+fn add_rest_framework(complete_path_project: &PathBuf, project_name: &String)->Result<(), Box<dyn std::error::Error>>{
+    println!("Installing REST framework...");
+        
+    if let Err(error) = exect_commands(
+        build_command_install_rest_framework(&complete_path_project)
+    ) {
+        return Err(error);
+    }
+    
+    let resutl = add_rest_framework_to_installed_apps(&complete_path_project, project_name);
+    
+    match resutl {
+        Ok(_) => println!("'rest_framework' added to INSTALLED_APPS in settings.py."),
+        Err(error) => {
+            println!("Add 'rest_framework' manually.");
+            return Err(error);
+        }
+    }
+    
 
+    Ok(())
+}
+
+fn add_rest_framework_to_installed_apps(complete_path_project: &PathBuf, project_name: &String) -> Result<(), Box<dyn std::error::Error>> {
+
+    let settings_path = complete_path_project.join(project_name).join("settings.py");
+    let settings_content = fs::read_to_string(&settings_path)?;
+
+    let installed_apps_pos = settings_content.find("INSTALLED_APPS");
+    let installed_apps_pos = installed_apps_pos.ok_or("INSTALLED_APPS not found in settings.py file.")?;
+
+
+    let bracket_pos = settings_content[installed_apps_pos..]
+        .find(']')
+        .map(|pos| installed_apps_pos + pos)
+        .ok_or("Closing bracket not found for INSTALLED_APPS.")?;
+
+
+    let mut modified_content = String::with_capacity(settings_content.len() + 20);
+    modified_content.push_str(&settings_content[..bracket_pos]);
+    modified_content.push_str("    'rest_framework',\n");
+    modified_content.push_str(&settings_content[bracket_pos..]);
+
+    fs::write(&settings_path, modified_content)?;
+
+    Ok(())
+}
